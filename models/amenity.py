5844af04ab28f0710cd9a62db1ef96aa0f9cc4e8
walindi <walindibarack@yahoo.com>
2023-05-21 21:58:12 +0300

    Minor fix

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """ Amenities for database """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
