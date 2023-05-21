016f6eb1fdae2faf79e712c63dce78ca3ed1f11b
walindi <walindibarack@yahoo.com>
2023-05-22 00:14:19 +0300

    Minor fix

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,13 @@
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 if getenv("HBNB_TYPE_STORAGE") == "db":
